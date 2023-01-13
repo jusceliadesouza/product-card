54950de9f0e98317154479b9c4a7bb6c48dd7cc5
Juscélia de Souza <jusceliadesousa@gmail.com>
2023-01-12 21:22:42 -0300

    configurações iniciais

---
+++ web/src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div>
+      <span className="product-code">Código: 42404</span>
+      <h1 className="title">Sofá Margot II - Rosé</h1>
+      <p className="price">R$ 4.000</p>
+    </div>
+  );
+}
+
+export default App;
---
+++ web/tailwind.config.cjs
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.tsx"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#D9CDF7",
+        secondary: "#271A45",
+      },
+    },
+  },
+  plugins: [],
+};
