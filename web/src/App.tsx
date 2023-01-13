61e357b25907e9085a134695189c9ad07efd9ec9
Juscélia de Souza <jusceliadesousa@gmail.com>
2023-01-12 22:38:53 -0300

    estrutura do card

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,9 +1,31 @@
+import product from "./assets/sofa.svg";
+import vector from "./assets/vector.svg";
+
 function App() {
   return (
-    <div>
-      <span className="product-code">Código: 42404</span>
-      <h1 className="title">Sofá Margot II - Rosé</h1>
-      <p className="price">R$ 4.000</p>
+    <div className="product">
+      <div className="product-image">
+        <button className="ml-[81%]">
+          <img
+            src={vector}
+            className="vector"
+            alt="Ativar visão 360° do produto"
+          />
+        </button>
+
+        <img src={product} className="image" alt="Sofá na cor rosé" />
+      </div>
+
+      <div className="product-info">
+        <div className="text">
+          <span className="code">Código: 42404</span>
+          <h1 className="title">Sofá Margot II - Rosé</h1>
+          <p className="price">R$ 4.000</p>
+        </div>
+
+        <button className="button">Adicionar à cesta</button>
+
+      </div>
     </div>
   );
 }
