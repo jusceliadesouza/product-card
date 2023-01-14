19b47e15c9f18fa9fde83cb1b6493464d5e2ccc2
Juscélia de Souza <jusceliadesousa@gmail.com>
2023-01-14 00:09:00 -0300

    correção da lógica do botão

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -5,6 +5,7 @@ import ProductImage from "./assets/sofa.svg";
 import Product3D from './assets/sofa.gif';
 import Vector from "./assets/vector.svg";
 
+
 const product = {
   code: 42404,
   title: "Sofá Margot II - Rosé",
@@ -22,18 +23,18 @@ function App() {
   return (
     <div className="product">
       <div className="product-image">
-        <button type="button" title={isActive ? "Visão 360°" : "Fechar"} onClick={handleActiveVision}>
-          <img src={isActive ? Vector : Close} alt={isActive ? "Visão 360°" : "Fechar"} />
+        <button type="button" title={isActive ? "Fechar" : "Visão 360°"} onClick={handleActiveVision}>
+          <img src={isActive ? Close : Vector} alt={isActive ? "Visão 360°" : "Fechar"} />
         </button>
 
-        <img src={isActive ? ProductImage : Product3D} alt={product.alt} />
+        <img src={isActive ? Product3D  : ProductImage} alt={product.alt} />
       </div>
 
       <div className="product-info">
         <div className="text">
           <span className="code">Código: {product.code}</span>
           <h1 className="title">{product.title}</h1>
-          <p className="price">R$ {product.price}</p>
+          <p className="price">{product.price}</p>
         </div>
 
         <button className="button" onClick={handleActiveVision}>
