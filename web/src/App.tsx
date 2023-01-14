edaa45eee12a1168dbd45b559f23bbd0cc006296
Juscélia de Souza <jusceliadesousa@gmail.com>
2023-01-13 23:40:58 -0300

    funcionalidades do button

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,30 +1,44 @@
-import product from "./assets/sofa.svg";
-import vector from "./assets/vector.svg";
+import { useState } from "react";
+
+import Close from './assets/close.svg';
+import ProductImage from "./assets/sofa.svg";
+import Product3D from './assets/sofa.gif';
+import Vector from "./assets/vector.svg";
+
+const product = {
+  code: 42404,
+  title: "Sofá Margot II - Rosé",
+  price: 4000,
+  alt: "Sofá na cor rosé",
+};
 
 function App() {
+  const [isActive, setIsActive] = useState(false);
+
+  function handleActiveVision() {
+    {isActive ? setIsActive(false) : setIsActive(true)}
+  }
+
   return (
     <div className="product">
       <div className="product-image">
-        <button className="ml-[81%]">
-          <img
-            src={vector}
-            className="vector"
-            alt="Ativar visão 360° do produto"
-          />
+        <button type="button" title={isActive ? "Visão 360°" : "Fechar"} onClick={handleActiveVision}>
+          <img src={isActive ? Vector : Close} alt={isActive ? "Visão 360°" : "Fechar"} />
         </button>
 
-        <img src={product} className="image" alt="Sofá na cor rosé" />
+        <img src={isActive ? ProductImage : Product3D} alt={product.alt} />
       </div>
 
       <div className="product-info">
         <div className="text">
-          <span className="code">Código: 42404</span>
-          <h1 className="title">Sofá Margot II - Rosé</h1>
-          <p className="price">R$ 4.000</p>
+          <span className="code">Código: {product.code}</span>
+          <h1 className="title">{product.title}</h1>
+          <p className="price">R$ {product.price}</p>
         </div>
 
-        <button className="button">Adicionar à cesta</button>
-
+        <button className="button" onClick={handleActiveVision}>
+          Adicionar à cesta
+        </button>
       </div>
     </div>
   );
--- web/tailwind.config.cjs
+++ web/tailwind.config.cjs
@@ -7,6 +7,10 @@ module.exports = {
         primary: "#D9CDF7",
         secondary: "#271A45",
       },
+      backgroundImage: {
+        'visionButton' : "url('../assets/vector.svg')",
+        'closeButton': "url('../assets/close.svg')",
+      }
     },
   },
   plugins: [],
